634fa0361293e8b50d919a3748c9a2b0174da949
vicluthfi <muhammad.alghifari@students.amikom.ac.id>
2020-03-30 23:34:01 +0700

    The Coffe Mechine

---
+++ TheCoffeMechine/CoffeMechine.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoffeMechine
+{
+    class CoffeMechine
+    {
+        private CoffePowder coffePowder;
+        private WaterGalon waterGalon;
+        private Milk milks;
+
+        public CoffeMechine(CoffePowder powder, WaterGalon galon, Milk milk)
+        {
+            this.waterGalon = galon;
+            this.coffePowder = powder;
+            this.milks = milk;
+        }
+
+        public String makeEsspresso()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            return "Yey! your Esspreso coffe is ready";
+        }
+
+        public String makeCappucino()
+        {
+            if (!this.waterGalon.isAvailable())
+            {
+                return "Sorry, the water is empty";
+            }
+            if (!this.coffePowder.isAvailable())
+            {
+                return "Sorry, the coffe is empty";
+            }
+            if (!this.milks.isAvailable())
+            {
+                return "Sorry, the milk is empty";
+            }
+
+            this.waterGalon.makeOneCup();
+            this.coffePowder.makeOneCup();
+            this.milks.makeOneCup();
+            return "Yey! your Cappucino coffe is ready";
+        }
+
+        public String checkAvailability()
+        {
+            return $"the water :{this.waterGalon.getVolume()} , the coffe powder: { this.coffePowder.getNetto()}, and the milk : {this.milks.getVolume()}";
+        }
+    }
+}
